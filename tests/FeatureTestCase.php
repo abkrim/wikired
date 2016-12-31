3443a2ff8a66650136003dc84a595ad707ef4e9a
Abdelkarim Mateos <abdelkarim@tamainut.com>
2016-12-31 08:56:21 +0100

    Primera subida

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+/**
+ * Este fichero es parte de Cprsync.
+ *
+ * @version 3.0.1
+ * @link https://tamainut.com/
+ * @autor AbdelKarim Mateos
+ * @copyright Copyright (C) 2016 Abdelkarim Mateos. All rights reserved.
+ * @license Comercial license
+ * @package \\\
+ */
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
---
+++ tests/features/ExampleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends FeatureTestCase
+{
+    //use DatabaseMigrations;
+    // Ya esta en la clase extendida
+    // use DatabaseTransactions;  // usa transacciones por lo que alfinal del test no se ejecuta contenido
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    function test_basic_example()
+    {
+        $name   = 'AbdelKarim Mateos';
+        $email  = 'abdelkarim@tamainut.com';
+
+        $user = factory(\App\User::class)->create([
+            'name'  => $name,
+            'email' => $email,
+        ]);
+
+        $this->actingAs($user, 'api')
+             ->visit('api/user')
+             ->see($name)
+             ->see($email);
+    }
+}
